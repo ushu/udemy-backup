6c91ffc97799680f7dc9e22a39a6a80bfce4a9f5
Aurélien Noce <aurelien@noce.fr>
2018-12-27 10:59:32 +0100

    Update CLI to use standard flags

--- main.go
+++ main.go
@@ -1,23 +1,54 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
-	"github.com/ushu/udemy-backup/cmd"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
+const Version = "0.2.0"
+const About = `Make backups of Udemy course contents for online usage.`
+
+// Flag values
+var (
+	showHelp    bool
+	showVersion bool
+	downloadAll bool
+	quiet bool
+)
+
+func init() {
+	flag.BoolVar(&showHelp, "help", false, "show usage info")
+	flag.BoolVar(&showVersion, "version", false, "show version number")
+	flag.BoolVar(&downloadAll, "all", false, "download all the courses enrolled by the user")
+	flag.BoolVar(&quiet, "quiet", false, "disable output messages")
+	flag.Usage = usage
+}
+
 func main() {
-	cmd.Execute()
+	log.SetFlags(0)
+	log.SetPrefix("[udemy-backup] ")
+	flag.Parse()
+	if showHelp {
+		usage()
+		return
+	}
+	if showVersion {
+		fmt.Printf("%s v%s\n", os.Args[0], Version)
+		return
+	}
+	if quiet {
+		log.SetOutput(ioutil.Discard)
+	}
+}
+
+func usage() {
+	fmt.Printf("Usage: %s [COURSE URL|COURSE ID]", os.Args[0])
+	fmt.Print("\n\n")
+	fmt.Printf(About)
+	fmt.Print("\n\n")
+	fmt.Println("OPTIONS:")
+	flag.PrintDefaults()
 }
