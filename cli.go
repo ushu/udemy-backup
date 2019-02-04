0a96c637f80fc546084e97db81009d2ab7760cc7
Aurélien Noce <aurelien@noce.fr>
2019-02-04 17:12:17 +0100

    Update email regexp

--- cli.go
+++ cli.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"errors"
+	"regexp"
 
 	"github.com/manifoldco/promptui"
 	"github.com/ushu/udemy-backup/client"
 )
+
+// inspired from taken verbatim from http://www.golangprograms.com/regular-expression-to-validate-email-address.html
+var emailRegexp = regexp.MustCompile("(?i)^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,8}$")
 
 // selectCourse allows to select a course among a previously-downloaded list
 func selectCourse(courses []*client.Course) (*client.Course, error) {
@@ -36,7 +40,7 @@ func selectCourse(courses []*client.Course) (*client.Course, error) {
 func askCredentials() (email string, password string, err error) {
 	prompt := promptui.Prompt{
 		Label:    "Email",
-		Validate: notEmpty,
+		Validate: isEmail,
 	}
 	email, err = prompt.Run()
 	if err != nil {
@@ -56,6 +60,13 @@ func askCredentials() (email string, password string, err error) {
 		err = errors.New("password is required")
 	}
 	return
+}
+
+func isEmail(s string) error {
+	if !emailRegexp.MatchString(s) {
+		return errors.New("not a valid email")
+	}
+	return nil
 }
 
 func notEmpty(s string) error {
